eeaf91f91fffebe31a38e03b85191655cf9cf723
Danzrrr <graita.sukma@gmail.com>
2024-12-18 23:17:25 +0700

    refactor: update NFTMarketplace to use address type for NFT mappings and improve error handling

--- src/NFTMarketplace.sol
+++ src/NFTMarketplace.sol
@@ -13,37 +13,41 @@ contract NFTMarketplace is IERC721Receiver {
         address seller;
     }
 
-    mapping(IERC721 => mapping(uint256 => NFTSale)) public nftSales;
+    mapping(address => mapping(uint256 => NFTSale)) public nftSales;
 
     error AccountError(string message);
     error NFTError(string message);
 
-    event NFTSaleCreated(IERC721 indexed nft, address seller, uint256 indexed tokenId, uint256 price);
-    event NFTSold(IERC721 indexed nft, address seller, address buyer, uint256 indexed tokenId, uint256 price);
-    event NFTWithdrawn(IERC721 indexed nft, address seller, uint256 indexed tokenId);
+    event NFTSaleCreated(address indexed nft, address seller, uint256 indexed tokenId, uint256 price);
+    event NFTSold(address indexed nft, address seller, address buyer, uint256 indexed tokenId, uint256 price);
+    event NFTWithdrawn(address indexed nft, address seller, uint256 indexed tokenId);
 
-    function withdraw(IERC721 _nft, uint256 _tokenId) external {
-        if (nftSales[IERC721(_nft)][_tokenId].seller != msg.sender) {
+    function withdraw(address _nft, uint256 _tokenId) external {
+        if (nftSales[_nft][_tokenId].seller != msg.sender) {
             revert AccountError("You are not the seller of this NFT");
         }
 
-        _nft.safeTransferFrom(address(this), msg.sender, _tokenId);
-        delete nftSales[IERC721(_nft)][_tokenId];
+        delete nftSales[_nft][_tokenId];
+        IERC721(_nft).safeTransferFrom(address(this), msg.sender, _tokenId);
         emit NFTWithdrawn(_nft, msg.sender, _tokenId);
     }
 
-    function buy(IERC721 _nft, uint256 _tokenId) external payable {
+    function buy(address _nft, uint256 _tokenId) external payable {
         NFTSale memory sale = nftSales[_nft][_tokenId];
         if (sale.seller == address(0)) {
-            revert AccountError("NFT is not for sale");
+            revert NFTError("NFT is not for sale");
         }
         if (msg.value < sale.price) {
             revert AccountError("Insufficient funds");
         }
 
         delete nftSales[_nft][_tokenId];
-        payable(sale.seller).transfer(msg.value);
-        _nft.safeTransferFrom(sale.seller, msg.sender, _tokenId);
+        (bool success,) = sale.seller.call{ value: sale.price }("");
+        if (!success) {
+            revert AccountError("Failed to send funds to seller");
+        }
+
+        IERC721(_nft).safeTransferFrom(address(this), msg.sender, _tokenId);
         emit NFTSold(_nft, sale.seller, msg.sender, _tokenId, sale.price);
     }
 
@@ -60,16 +64,15 @@ contract NFTMarketplace is IERC721Receiver {
         console2.log(_operator); // Remove warning variable not used :D
         if (!IERC721(msg.sender).supportsInterface(type(IERC721).interfaceId)) {
             // Check if the contract supports ERC721 interface
-            revert AccountError("Contract does not support ERC721 interface");
+            revert NFTError("Contract does not support ERC721 interface");
         }
         if (_data.length == 0) {
             revert NFTError("Price is required");
         }
         uint256 price = abi.decode(_data, (uint256));
-        console2.log(price); // Remove warning variable not used :D
-        nftSales[IERC721(msg.sender)][_tokenId] = NFTSale(price, _from);
+        nftSales[msg.sender][_tokenId] = NFTSale(price, _from);
 
-        emit NFTSaleCreated(IERC721(msg.sender), _from, _tokenId, price);
+        emit NFTSaleCreated(msg.sender, _from, _tokenId, price);
         return this.onERC721Received.selector;
     }
 }
--- test/NFTMarketplace.t.sol
+++ test/NFTMarketplace.t.sol
@@ -10,7 +10,8 @@ contract NFTMarketplaceTest is Test {
     SimpleNFT simpleNFT;
     AnotherNFT anotherNFT;
 
-    uint256 LIST_PRICE = 1e17; // 0.1 ETH
+    uint256 LIST_PRICE = 0.1 ether;
+    uint256 LOWER_BUY_PRICE = 0.05 ether;
 
     function setUp() public {
         nftMarketplace = new NFTMarketplace();
@@ -53,9 +54,34 @@ contract NFTMarketplaceTest is Test {
         bytes memory encodedPrice = abi.encode(LIST_PRICE);
         simpleNFT.safeTransferFrom(address(1), address(nftMarketplace), 1, encodedPrice);
         assertEq(simpleNFT.ownerOf(1), address(nftMarketplace));
-        (uint256 price, address seller) = nftMarketplace.nftSales(simpleNFT, 1);
+        (uint256 price, address seller) = nftMarketplace.nftSales(address(simpleNFT), 1);
         assertEq(price, LIST_PRICE);
         assertEq(seller, address(1));
+        vm.stopPrank();
+    }
+
+    function test_ListMultipleNFTs() public {
+        simpleNFT.safeTransferFrom(address(this), address(1), 1);
+        anotherNFT.safeTransferFrom(address(this), address(2), 1);
+        assertEq(simpleNFT.ownerOf(1), address(1));
+        assertEq(anotherNFT.ownerOf(1), address(2));
+
+        vm.startPrank(address(1));
+        bytes memory encodedPrice = abi.encode(LIST_PRICE);
+        simpleNFT.safeTransferFrom(address(1), address(nftMarketplace), 1, encodedPrice);
+        assertEq(simpleNFT.ownerOf(1), address(nftMarketplace));
+        (uint256 price, address seller) = nftMarketplace.nftSales(address(simpleNFT), 1);
+        assertEq(price, LIST_PRICE);
+        assertEq(seller, address(1));
+        vm.stopPrank();
+
+        vm.startPrank(address(2));
+        anotherNFT.safeTransferFrom(address(2), address(nftMarketplace), 1, encodedPrice);
+        assertEq(anotherNFT.ownerOf(1), address(nftMarketplace));
+        (price, seller) = nftMarketplace.nftSales(address(anotherNFT), 1);
+        assertEq(price, LIST_PRICE);
+        assertEq(seller, address(2));
+        vm.stopPrank();
     }
 
     function testFail_WithdrawNFTWithNonSeller() public {
@@ -63,7 +89,62 @@ contract NFTMarketplaceTest is Test {
         assertEq(simpleNFT.ownerOf(1), address(1));
 
         vm.startPrank(address(1));
-        nftMarketplace.withdraw(simpleNFT, 1);
+        nftMarketplace.withdraw(address(simpleNFT), 1);
+        vm.stopPrank();
+    }
+
+    function test_WithdrawNFT() public {
+        simpleNFT.safeTransferFrom(address(this), address(1), 1);
+        assertEq(simpleNFT.ownerOf(1), address(1));
+
+        vm.startPrank(address(1));
+        bytes memory encodedPrice = abi.encode(LIST_PRICE);
+        simpleNFT.safeTransferFrom(address(1), address(nftMarketplace), 1, encodedPrice);
+        assertEq(simpleNFT.ownerOf(1), address(nftMarketplace));
+
+        nftMarketplace.withdraw(address(simpleNFT), 1);
+        assertEq(simpleNFT.ownerOf(1), address(1));
+        vm.stopPrank();
+    }
+
+    function testFail_BuyNFTWithoutListed() public {
+        vm.startPrank(address(2));
+        nftMarketplace.buy(address(simpleNFT), 1);
+        vm.stopPrank();
+    }
+
+    function testFail_BuyNFTWithInsufficientFunds() public {
+        simpleNFT.safeTransferFrom(address(this), address(1), 1);
+        assertEq(simpleNFT.ownerOf(1), address(1));
+
+        vm.startPrank(address(1));
+        bytes memory encodedPrice = abi.encode(LIST_PRICE);
+        simpleNFT.safeTransferFrom(address(1), address(nftMarketplace), 1, encodedPrice);
+        assertEq(simpleNFT.ownerOf(1), address(nftMarketplace));
+        vm.stopPrank();
+
+        vm.startPrank(address(2));
+        nftMarketplace.buy{ value: LOWER_BUY_PRICE }(address(simpleNFT), 1);
+        vm.stopPrank();
+    }
+
+    function test_BuyNFT() public {
+        simpleNFT.safeTransferFrom(address(this), address(1), 1);
+        assertEq(simpleNFT.ownerOf(1), address(1));
+
+        vm.startPrank(address(1));
+        bytes memory encodedPrice = abi.encode(LIST_PRICE);
+        simpleNFT.safeTransferFrom(address(1), address(nftMarketplace), 1, encodedPrice);
+        assertEq(simpleNFT.ownerOf(1), address(nftMarketplace));
+        vm.stopPrank();
+
+        vm.startPrank(address(2));
+        vm.deal(address(2), 1 ether);
+        uint256 sellerBalanceBefore = address(1).balance;
+        nftMarketplace.buy{ value: 0.1 ether }(address(simpleNFT), 1);
+        assertEq(simpleNFT.ownerOf(1), address(2));
+        uint256 sellerBalanceAfter = address(1).balance;
+        assertEq(sellerBalanceBefore, sellerBalanceAfter - LIST_PRICE);
         vm.stopPrank();
     }
 }
